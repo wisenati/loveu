1f475c6ab560eae86f0692b79ca4667ef55f213c
wisenati <aladefavour134@gmail.com>
2025-05-06 13:01:44 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,134 @@
+// Cache name version
+const CACHE_NAME = "love-u-cache-v1";
+
+// Files to cache
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/registration.html",
+  "/manifest.json",
+  "/styles/main.css",
+  "/scripts/app.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/webfonts/fa-solid-900.woff2",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/webfonts/fa-regular-400.woff2",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+  "/icons/maskable-icon.png",
+];
+
+// Install service worker and cache all content
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              // Delete old caches
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+// Fetch event - serve from cache or network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return (
+          response ||
+          fetch(event.request).then((fetchResponse) => {
+            // Don't cache CORS or third-party requests
+            if (
+              !fetchResponse ||
+              fetchResponse.status !== 200 ||
+              fetchResponse.type !== "basic"
+            ) {
+              return fetchResponse;
+            }
+
+            // Clone the response
+            const responseToCache = fetchResponse.clone();
+
+            // Add to cache for next time
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+            return fetchResponse;
+          })
+        );
+      })
+      .catch(() => {
+        // If both cache and network fail, show offline page
+        if (event.request.url.indexOf(".html") > -1) {
+          return caches.match("/offline.html");
+        }
+      })
+  );
+});
+
+// Handle offline page
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+// Handle push notifications (for future implementation)
+self.addEventListener("push", (event) => {
+  if (event.data) {
+    const notificationData = event.data.json();
+    const options = {
+      body: notificationData.body,
+      icon: "/icons/icon-192x192.png",
+      badge: "/icons/badge-72x72.png",
+      vibrate: [100, 50, 100],
+      data: {
+        url: notificationData.url,
+      },
+    };
+
+    event.waitUntil(
+      self.registration.showNotification(notificationData.title, options)
+    );
+  }
+});
+
+// Notification click handler
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  if (event.notification.data && event.notification.data.url) {
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+  }
+});
